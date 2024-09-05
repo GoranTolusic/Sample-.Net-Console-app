91c035d013b6c5c28221e99cb84ee596897e04ef
GoranTolusic <goran.tolusic@gmail.com>
2024-09-05 17:32:39 +0200

    elastic request for creating indexes according to db models

--- Services/ElasticSearchService.cs
+++ Services/ElasticSearchService.cs
@@ -2,6 +2,12 @@ using EFConsoleApp.Data;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Dynamic;
+using System.Linq;
+
 
 namespace ElasticSearch
 {
@@ -29,22 +35,72 @@ namespace ElasticSearch
             }
         }
 
+        public async Task ElasticCreateIndex(string indexName, string bodyObject)
+        {
+
+            // Strip namespaces and convert to lower cases
+            int lastDotIndex = indexName.LastIndexOf('.');
+            string actualIndexName = indexName.Substring(lastDotIndex + 1).ToLower();
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"http://127.0.0.1:9200/{actualIndexName}";
+                Console.WriteLine($"url sa put metodom i indexom: {url}");
+
+                // Kreiramo HttpContent sa postavljenim Content-Type headerom
+                HttpContent content = new StringContent(bodyObject, Encoding.UTF8, "application/json");
+
+                // Slanje PUT zahtjeva na url
+                HttpResponseMessage response = await client.PutAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response: {responseBody}");
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+        }
+
         public async Task CreateIndexes(SchoolContext context)
         {
             await this.ElasticRequest();
-            //Scan models for props and keys.
+
             var model = context.Model;
             foreach (var entityType in model.GetEntityTypes())
             {
-                //TODO: na temelju ovih podataka napraviti requestove prema elastic api-u i kreirati index modele za studenta i profesora
-
                 Console.WriteLine($"Entity: {entityType.Name}");
+                // Dinamično kreiranje JSON strukture za ElasticSearch
+                dynamic elasticJson = new ExpandoObject();
+                elasticJson.settings = new ExpandoObject();
+                elasticJson.settings.number_of_shards = 1;
+                elasticJson.settings.number_of_replicas = 1;
+
+                elasticJson.mappings = new ExpandoObject();
+                elasticJson.mappings.properties = new ExpandoObject();
 
                 Console.WriteLine("Properties:");
                 foreach (var property in entityType.GetProperties())
                 {
                     Console.WriteLine($" - {property.Name} ({property.ClrType.Name})");
+                    string elasticType = GetElasticSearchType(property.ClrType);
+
+                    // Dodajemo property u mappings koristeći IDictionary interface za ExpandoObject
+                    var propertyDict = (IDictionary<string, object>)elasticJson.mappings.properties;
+                    propertyDict[property.Name] = new ExpandoObject();
+                    var typeDict = (IDictionary<string, object>)propertyDict[property.Name];
+                    typeDict["type"] = elasticType; // Postavljanje tipa                    
                 }
+
+                // Serijalizacija dinamičkog objekta u JSON i poziv na elastic API
+                string jsonBody = JsonSerializer.Serialize(elasticJson, new JsonSerializerOptions { WriteIndented = true });
+                Console.WriteLine(jsonBody);
+                await this.ElasticCreateIndex(entityType.Name, jsonBody);
+
+
 
                 Console.WriteLine("Keys:");
                 foreach (var key in entityType.GetKeys())
@@ -61,6 +117,34 @@ namespace ElasticSearch
                 Console.WriteLine();
             }
 
+        }
+
+        static string GetElasticSearchType(Type clrType)
+        {
+            if (clrType == typeof(string))
+            {
+                return "text"; // ili "keyword" ovisno o scenariju
+            }
+            else if (clrType == typeof(int) || clrType == typeof(long))
+            {
+                return "integer";
+            }
+            else if (clrType == typeof(DateTime))
+            {
+                return "date";
+            }
+            else if (clrType == typeof(bool))
+            {
+                return "boolean";
+            }
+            else if (clrType == typeof(float) || clrType == typeof(double) || clrType == typeof(decimal))
+            {
+                return "float";
+            }
+            else
+            {
+                return "text"; // Defaultni tip
+            }
         }
     }
 }
